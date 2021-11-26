010e5bf3d26ffdf62cbec530ac9649235ef3fab6
Abhishek kumar <itachi0uchiha19@gmail.com>
2021-11-26 23:38:31 +0530

    fix(workflow rules): Added the Semantic Release.

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/itachiuchihadev/react-app-wokflow",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github"
+      ]
+}
